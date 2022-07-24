90cad21af782e385a4f5110c8cbc4228ba534f7c
aomona <104058728+aomona@users.noreply.github.com>
2022-07-24 20:10:47 +0900

    aaaaa

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const container = document.querySelector('.container')
 container.addEventListener('animationend', () => {
   container.classList.remove('active');
 });
+
+
+lottie.loadAnimation({
+  container: document.getElementById('lottie'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '106680-login-and-sign-up.json' // the path to the animation json
+});
