a69d156a63fe9ca3bab0e178f295fdf9c9eeb027
aomona <104058728+aomona@users.noreply.github.com>
2022-07-26 14:47:23 +0900

    インデックスドットPHPを追加したよ

--- index.php
+++ index.php
@@ -1,2 +1,33 @@
-<?php if(isset($_GET['user_id'])) { $user_id = $_GET['user_id']; } ?>
-<?php echo $user_id; ?>
+<?php
+if($_POST["pass"] == "manager"){
+  header('Location: login_success_manager.php');
+exit;
+}
+if($_POST["pass"] == "user"){
+    header('Location: login_success_users.php');
+exit;
+}
+?>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>ログイン</title>
+  <link rel="stylesheet" href="index.css" type="text/css">
+ </head>
+ <body>
+    <h1>ログインページ</h1>
+    <h2>ログイン</h2>
+    <form name="login_form">
+      <div class="login_form_top">
+        <h1>LOGIN</h1>
+        <p>UserID、Passwordをご入力の上、「LOGIN」ボタンをクリックしてください。</p>
+      </div>
+      <div class="login_form_btm" method="POST">
+        <input type="id" name="user_id" placeholder="あいでぃー">
+        <input type="text" name="pass" placeholder="ぱすわーど">
+        <input type="submit" value="ログイン">
+      </div>
+    </form>
+</form>
+ </body>
+</html> 
